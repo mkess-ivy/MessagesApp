aadb13eb1de18965309a3db99420f0aac96ea9d3
mkess-ivy <montier.kess@gmail.com>
2015-08-26 14:29:21 -0400

    not able to get db to include seeds.rb, errors :(

--- db/migrate/20150826174117_create_messages.rb
+++ db/migrate/20150826174117_create_messages.rb
@@ -1,8 +1,8 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-    	t.text :content
-      t.timestamps null: false
+      t.text :content
+      t.timestamps
     end
   end
 end
